31571eaa87b842855a02932ac3bb0d5352f80301
D-tarev <lintarevdima@gmail.com>
2024-10-12 14:37:02 +0300

    Update

---
+++ src/api/api.js
@@ -0,0 +1,46 @@
+const getData = async () => {
+  const response = await fetch("http://localhost:8081/productTypes").then(
+    (response) => response.json()
+  );
+  return response;
+};
+
+const deleteData = async (prodId) => {
+  const response = await fetch(`http://localhost:8081/productTypes/${prodId}`, {
+    method: "DELETE",
+  });
+  return response;
+};
+
+const patchData = async (prodId, data = {}) => {
+  const response = await fetch(`http://localhost:8081/productTypes/${prodId}`, {
+    method: "PATCH",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+
+const postData = async (data = {}) => {
+  const response = await fetch("http://localhost:8081/productTypes", {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+const getProductById = async (prodId) => {
+  const response = await fetch(
+    `http://localhost:8081/productTypes/${prodId}`
+  ).then((response) => response.json());
+  return response;
+};
+export { deleteData, patchData, postData, getProductById, getData };
